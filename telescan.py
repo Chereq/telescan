029802454c7d42f12a823794e030e376c159ab32
Chereq <mr.dead.snyper@gmail.com>
2022-06-24 20:54:56 +0300

    export chat members by chat_link added

--- telescan.py
+++ telescan.py
@@ -20,7 +20,7 @@ print(Fore.CYAN + """
    ██║   ███████╗███████╗███████╗███████║╚██████╗██║  ██║██║ ╚████║
    ╚═╝   ╚══════╝╚══════╝╚══════╝╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝
 --- v1.0 by @Pielco11 ---
-""")
+""" + Style.RESET_ALL)
 
 def userID(userInput):
     if userInput.startswith("pn-") or userInput.startswith("u-"):
@@ -40,7 +40,7 @@ def chatListPrint(data):
     _username = data['username']
     print("[id] {} | Title: {} | Username: {} | Type: {} | DC: {}".format(_id, _title, _username, _type, _dc))
 
-def singleUserLookup(user):
+def singleUserLookup(user, highlight=True):
     _id = user['user']['id']
     _contact = user['user']['is_contact']
     _first_name = user['user']['first_name']
@@ -52,12 +52,18 @@ def singleUserLookup(user):
     _lod = user['user']['last_online_date']
     if _lod:
         _lod = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(_lod))
-    print("------\n|-> ID: {}\n|-> Contact: {}\n|-> Full name: {}\n|-> Username: {}\n|-> Phone: {}".format(_id, _contact, _fullName, _username, _phone))
-    print("|-> DC: {}\n|-> Last Online Date: {}".format(_dc, _lod))
+    print("------")
+    print(f"|-> ID: {_id}")
+    print(f"|-> Contact: {_contact}")
+    print(f"|-> Full name: {_fullName}")
+    print((Fore.GREEN if _username else '') + f"|-> Username: {_username}" + Style.RESET_ALL)
+    print((Fore.GREEN if _phone else '') + f"|-> Phone: {_phone}" + Style.RESET_ALL)
+    print(f"|-> DC: {_dc}")
+    print(f"|-> Last Online Date: {_lod}", end='')
 
 
 
-def chatMembersInfoPrint(data, total=True):
+def chatMembersInfoPrint(data, total=True, highlight=True):
     if total:
         totalCount = len(data)
         print("[+++] Users count: {}".format(totalCount))
@@ -66,7 +72,7 @@ def chatMembersInfoPrint(data, total=True):
         userList = data
     print(Fore.GREEN + "[user infos]" + Style.RESET_ALL)
     for user in userList:
-        singleUserLookup(user)
+        singleUserLookup(user, highlight)
         _status = user['status']
         _date = None
         try:
@@ -89,10 +95,12 @@ def chatMembersInfoPrint(data, total=True):
 
 with app:
     while True:
-        choice = input(Fore.CYAN + "[1] => chats lookup\n[2] => users lookup\n" + 
+        print(Fore.CYAN + "[1] => chats lookup\n[2] => users lookup\n" + 
                                    "[3] => search user in groups\n" +
+                                   "[4] => public chats lookup\n"
                                    "[anything else] => exit\n" +
                                    "[<] " + Style.RESET_ALL)
+        choice = input()
         if choice == "1":
             dialogs = app.get_dialogs()
             i = 0
@@ -100,27 +108,34 @@ with app:
                 chatListPrint(d['chat'])
                 i += 1
                 if i >= 10:
-                    cmd = input(Fore.CYAN + "[(c)/x]: " + Style.RESET_ALL)
+                    print(Fore.CYAN + "[(c)/x]: " + Style.RESET_ALL)
+                    cmd = input()
                     i = 0
                     if cmd == 'x':
                         break
-            chatID = input(Fore.CYAN + "[chat lookup]: " + Style.RESET_ALL)
-            choice = input(Fore.CYAN + "[1] => Bulk search\n[2] => single user lookup\n[<]: " + Style.RESET_ALL)
+            print(Fore.CYAN + "[chat lookup]: " + Style.RESET_ALL)
+            chatID = input()
+            print(Fore.CYAN + "[1] => Bulk search\n[2] => single user lookup\n[<]: " + Style.RESET_ALL)
+            choice = input()
             if choice == "1":
-                limit = input(Fore.CYAN + "[# of users]: " + Style.RESET_ALL)
+                print(Fore.CYAN + "[# of users]: " + Style.RESET_ALL)
+                limit = input()
                 members = app.get_chat_members(chat_id=int(chatID), limit=int(limit))
                 chatMembersInfoPrint(members)
             elif choice == "2":
-                userInput = input(Fore.CYAN + "[user-id (id)/username (u)/phone number (pn)]: " + Style.RESET_ALL)
+                print(Fore.CYAN + "[user-id (id)/username (u)/phone number (pn)]: " + Style.RESET_ALL)
+                userInput = input()
                 members = app.get_chat_member(chat_id=int(chatID), user_id=userID(userInput))
                 chatMembersInfoPrint(members, total=False)
         elif choice == "2":
-            userInput = input(Fore.CYAN + "[user-id (id)/username (u)/phone number (pn)]: " + Style.RESET_ALL)
+            print(Fore.CYAN + "[user-id (id)/username (u)/phone number (pn)]: " + Style.RESET_ALL)
+            userInput = input()
             lookupResult = app.get_users([userID(userInput)])
             singleUserLookup({'user': lookupResult[0]})
         elif choice == "3":
             dialogs = app.get_dialogs()
-            userInput = input(Fore.CYAN + "[user-id (id)/username (u)/phone number (pn)]: " + Style.RESET_ALL)
+            print(Fore.CYAN + "[user-id (id)/username (u)/phone number (pn)]: " + Style.RESET_ALL)
+            userInput = input()
             for d in dialogs:
                 if int(d['chat']['id']) < 0:
                     try:
@@ -130,7 +145,15 @@ with app:
                     except BadRequest as e:
                         if not ("CHAT_ADMIN_REQUIRED" or "USER_NOT_PARTICIPANT" in str(e)):
                             print(str(e))
+        elif choice == "4":
+            print(Fore.CYAN + "[chat_link or name]: " + Style.RESET_ALL)
+            chatName = input()
+            print(Fore.CYAN + "[# of users]: " + Style.RESET_ALL)
+            limit = input()
+            chatData = app.get_chat(chatName)
+            members = app.get_chat_members(chat_id=chatData.id, limit=int(limit))
+            chatMembersInfoPrint(members)
         else:
-            print(Fore.RED + "\n[x] exiting!" +Style.RESET_ALL)
+            print(Fore.RED + "\n[x] exiting!" + Style.RESET_ALL)
             app.stop()
             exit()
